3418a0319881117eb52ef11be03100bce4ec374c
KiranrajPillai <kiranrajpillai1997@gmail.com>
2021-09-27 19:45:35 +0530

    Convert Temperature to Fahrenheit.

---
+++ src/com/Problems/TemperatureConversation.java
@@ -0,0 +1,18 @@
+package com.Problems;
+
+import java.util.Scanner;
+
+public class TemperatureConversation {
+    public static void main(String arg[])
+    {
+        double a;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter  Fahrenheit temperature");
+        a=sc.nextDouble();
+        System.out.println("Celsius temperature is = "+celsius(a));
+    }
+    static double celsius(double f)
+    {
+        return  (f-32)*5/9;
+    }
+}
